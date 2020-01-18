a27749afd06106225bb37e51d75933d8563c14c5
Chukwuemeka Inya <inyachukwuemeka1@gmail.com>
2020-01-18 05:02:27 +0100

    Modal bare styles setup

--- packages/picasso/src/Modal/index.js
+++ packages/picasso/src/Modal/index.js
@@ -1,33 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 const modalRoot = document.getElementById('docs-root');
+
+const BackDrop = styled.div`
+  display: ${props => (props.show ? 'flex' : 'none')};
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  position: fixed;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.6);
+  z-index: 900;
+`;
+
+const Container = styled.div`
+  width: 80%;
+  height: 80%;
+  background: white;
+  border: thin solid red;
+`;
+
+const ModalFrame = props => {
+  const { children, show } = props;
+  return (
+    <BackDrop show={show}>
+      <Container>{children}</Container>
+    </BackDrop>
+  );
+};
 
 class Modal extends React.Component {
   constructor(props) {
     super(props);
 
-    this.container = document.createElement('div');
+    this.wrapper = document.createElement('div');
   }
 
   componentDidMount() {
-    modalRoot.appendChild(this.container);
+    modalRoot.appendChild(this.wrapper);
   }
 
   componentWillUnmount() {
-    modalRoot.removeChild(this.container);
+    modalRoot.removeChild(this.wrapper);
   }
 
+  handleClose = () => {
+    const { onClose } = this.props;
+    onClose();
+  };
+
   render() {
-    const { children } = this.props;
-    const { container } = this;
-    return ReactDOM.createPortal(children, container);
+    const { children, show } = this.props;
+    const { wrapper, handleClose } = this;
+    return ReactDOM.createPortal(
+      <ModalFrame show={show} onClose={handleClose}>
+        {children}
+      </ModalFrame>,
+      wrapper
+    );
   }
 }
 
+ModalFrame.propTypes = {
+  children: PropTypes.node.isRequired,
+  show: PropTypes.bool
+};
+
+ModalFrame.defaultProps = {
+  show: false
+};
+
 Modal.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  show: PropTypes.bool,
+  onClose: PropTypes.func
+};
+
+Modal.defaultProps = {
+  onClose: () => {},
+  show: false
 };
 
 export default Modal;
--- packages/picasso/src/Modal/modal.stories.js
+++ packages/picasso/src/Modal/modal.stories.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from './index';
+import Button from '../Button';
 
 export default {
   title: 'Modal',
   component: Modal
 };
 
-export const BasicModal = () => (
-  <Modal>
-    <h1>Welcome Here</h1>
-    <p>Some random content!!!</p>
-  </Modal>
-);
+export const BasicModal = () => {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <Modal show={show}>
+        <h1>Welcome Here</h1>
+        <p>Some random content!!!</p>
+      </Modal>
+      <Button onClick={() => setShow(true)}>Show Modal</Button>
+    </>
+  );
+};
