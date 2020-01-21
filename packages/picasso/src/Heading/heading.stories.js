38ff80103d5ecbf67b05123116a8163b62d47a2f
Chukwuemeka Inya <inyachukwuemeka1@gmail.com>
2020-01-21 09:08:17 +0100

    Add Heading, Loading, Pane and Flex components

---
+++ packages/picasso/src/Heading/heading.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Heading from './index';
+
+export default {
+  title: 'Heading',
+  component: Heading
+};
+
+export const BasicHeading = () => (
+  <>
+    <Heading>This is a H1 heading</Heading>
+    <Heading as="h2">This is a H2 heading</Heading>
+    <Heading as="h3">This is a H3 heading</Heading>
+    <Heading as="h4">This is a H4 heading</Heading>
+    <Heading as="h5">This is a H5 heading</Heading>
+    <Heading as="h6">This is a H6 heading</Heading>
+  </>
+);
---
+++ packages/picasso/src/Heading/index.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import Text from '../Text';
+import themeGet from '../theme/utils';
+
+const supportedHeadings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+
+const fontSize = ({ as, theme }) => ({
+  'font-size': theme.headingSizes[supportedHeadings.indexOf(as)]
+});
+
+const Heading = styled(Text)`
+  padding: 0;
+  margin: 0;
+  color: ${themeGet('colors.black')};
+  font-weight: ${themeGet('fontWeights.6')};
+  ${fontSize}
+`;
+
+Heading.propTypes = {
+  as: PropTypes.oneOfType(supportedHeadings)
+};
+
+Heading.defaultProps = {
+  as: 'h1'
+};
+
+export default Heading;
