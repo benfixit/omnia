d43f5ab1f906f8f95107577828a8874039d704fc
Chukwuemeka Inya <inyachukwuemeka1@gmail.com>
2020-01-25 21:59:17 +0100

    Add Card component

--- packages/picasso/src/Card/index.js
+++ packages/picasso/src/Card/index.js
@@ -2,30 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import themeGet from '../theme/utils';
+
 const StyledCard = styled.div`
   display: flex;
   width: 125px;
-  height: 125px;
+  height: 64px;
   justify-content: center;
   align-items: center;
-  box-shadow: 2px 2px red;
+  border: thin solid ${themeGet('colors.lightGray')};
+  border-radius: 10px;
+  margin: 20px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.4) 0 1px 2px rgba(0, 0, 0, 0.4);
 `;
 
 const Card = props => {
-  const { title, ...rest } = props;
-  return (
-    <StyledCard {...rest}>
-      <h3>{title}</h3>
-    </StyledCard>
-  );
+  const { children, ...rest } = props;
+  return <StyledCard {...rest}>{children}</StyledCard>;
 };
 
 Card.propTypes = {
-  title: PropTypes.string
-};
-
-Card.defaultProps = {
-  title: ''
+  children: PropTypes.node.isRequired
 };
 
 export default Card;
