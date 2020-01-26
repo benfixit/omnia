e5dece8376c851860c14a9ff357b3cc1c0f5992a
Chukwuemeka Inya <inyachukwuemeka1@gmail.com>
2020-01-26 13:58:35 +0100

    Extract Transaction queries to hocs

--- packages/expense_tracker/src/components/Transactions.js
+++ packages/expense_tracker/src/components/Transactions.js
@@ -3,11 +3,14 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
 import { v4 } from 'uuid';
-import { graphql, Query } from 'react-apollo';
+import { graphql } from 'react-apollo';
 import compose from 'lodash/fp/compose';
 import Picasso from '@omnia/picasso';
-import { GET_CATEGORIES } from '../graphql/categories';
-import { GET_TRANSACTIONS, ADD_TRANSACTION } from '../graphql/transactions';
+
+import withCategoryQuery from '../hoc/withCategoryQuery';
+import withTransactionQuery from '../hoc/withTransactionQuery';
+import { ADD_TRANSACTION, GET_TRANSACTIONS } from '../graphql/transactions';
+import { formatter } from '../utils/money';
 import Layout from './Layout';
 import {
   Layout as LayoutStyle,
@@ -17,7 +20,6 @@ import {
   StyledButton,
   StyledHeading
 } from '../styles';
-import { monthsOfYear } from '../utils/date';
 
 const { DateField, InputField, Modal, SelectField, TextAreaField } = Picasso;
 
@@ -90,157 +92,102 @@ class Transactions extends Component {
       handleCloseModal,
       handleOpenModal
     } = this;
-    const dateParam = new Date();
-    const defaultYear = dateParam.getFullYear();
-    const {
-      match: {
-        params: { year = defaultYear, month }
-      }
-    } = this.props;
-    const monthIndex = monthsOfYear[month] || dateParam.getMonth();
+    const { categories, transactions } = this.props;
     return (
       <Layout>
         <TransactionsRow>
           <StyledHeading>Transactions</StyledHeading>
           <StyledButton onClick={handleOpenModal}>Add Transaction</StyledButton>
         </TransactionsRow>
-        <Query query={GET_CATEGORIES}>
-          {({
-            data: categoriesData,
-            error: categoriesError,
-            loading: categoriesLoading
-          }) => {
-            if (categoriesLoading) return <h2>Loading...</h2>;
-            if (categoriesError) return <h2>Error :(</h2>;
-            const { categories } = categoriesData;
-            return (
-              <>
-                <TransactionsRow>
-                  <Query
-                    query={GET_TRANSACTIONS}
-                    variables={{
-                      year: Number(year),
-                      month: Number(monthIndex)
-                    }}
-                  >
-                    {({
-                      data: transactionsData,
-                      loading: transactionsLoading,
-                      error: transactionsError
-                    }) => {
-                      if (transactionsLoading) return <span>Loading...</span>;
-                      if (transactionsError) return <span>Error :(</span>;
-                      const { transactions } = transactionsData;
-                      return (
-                        <Table.Table>
-                          <thead>
-                            <tr>
-                              <Table.Th>Date</Table.Th>
-                              <Table.Th>Description</Table.Th>
-                              <Table.Th>Category</Table.Th>
-                              <Table.Th>Amount</Table.Th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {transactions.map(transaction => {
-                              const transactionDate = new Date(
-                                Number(transaction.createdAt)
-                              );
-                              return (
-                                <tr key={v4()}>
-                                  <Table.Td>
-                                    {transactionDate.toLocaleDateString()}
-                                  </Table.Td>
-                                  <Table.Td>{transaction.description}</Table.Td>
-                                  <Table.Td>
-                                    {transaction.category.title}
-                                  </Table.Td>
-                                  <Table.Td>{transaction.amount}</Table.Td>
-                                </tr>
-                              );
-                            })}
-                          </tbody>
-                        </Table.Table>
-                      );
-                    }}
-                  </Query>
-                </TransactionsRow>
-                <TransactionsRow>
-                  <Modal show={showModal}>
-                    <Modal.Header title="Transaction" />
-                    <Modal.Content>
-                      <StyledForm onSubmit={handleSubmit}>
-                        <SelectField
-                          name="category"
-                          onChange={handleChange}
-                          value={category}
-                          label="Category"
-                        >
-                          <option value="">Select a category</option>
-                          {categories.map(item => {
-                            const { _id: id, title } = item;
-                            return (
-                              <option value={id} key={id}>
-                                {title}
-                              </option>
-                            );
-                          })}
-                        </SelectField>
-                        <InputField
-                          type="number"
-                          name="amount"
-                          value={amount}
-                          onChange={handleChange}
-                          label="Amount"
-                        />
-                        <DateField
-                          name="date"
-                          value={date}
-                          onChange={handleChange}
-                          label="period"
-                        />
-                        <TextAreaField
-                          name="description"
-                          value={description}
-                          onChange={handleChange}
-                          label="Description"
-                        />
-                        <FormButton type="submit">Submit</FormButton>
-                      </StyledForm>
-                    </Modal.Content>
-                    <Modal.Action onClose={handleCloseModal} />
-                  </Modal>
-                </TransactionsRow>
-              </>
-            );
-          }}
-        </Query>
+        <TransactionsRow>
+          <Table.Table>
+            <thead>
+              <tr>
+                <Table.Th>Date</Table.Th>
+                <Table.Th>Description</Table.Th>
+                <Table.Th>Category</Table.Th>
+                <Table.Th>Amount</Table.Th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map(transaction => {
+                const transactionDate = new Date(Number(transaction.createdAt));
+                return (
+                  <tr key={v4()}>
+                    <Table.Td>{transactionDate.toLocaleDateString()}</Table.Td>
+                    <Table.Td>{transaction.description}</Table.Td>
+                    <Table.Td>{transaction.category.title}</Table.Td>
+                    <Table.Td>{formatter.format(transaction.amount)}</Table.Td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table.Table>
+        </TransactionsRow>
+        <TransactionsRow>
+          <Modal show={showModal}>
+            <Modal.Header title="Transaction" />
+            <Modal.Content>
+              <StyledForm onSubmit={handleSubmit}>
+                <SelectField
+                  name="category"
+                  onChange={handleChange}
+                  value={category}
+                  label="Category"
+                >
+                  <option value="">Select a category</option>
+                  {categories.map(item => {
+                    const { _id: id, title } = item;
+                    return (
+                      <option value={id} key={id}>
+                        {title}
+                      </option>
+                    );
+                  })}
+                </SelectField>
+                <InputField
+                  type="number"
+                  name="amount"
+                  value={amount}
+                  onChange={handleChange}
+                  label="Amount"
+                />
+                <DateField
+                  name="date"
+                  value={date}
+                  onChange={handleChange}
+                  label="period"
+                />
+                <TextAreaField
+                  name="description"
+                  value={description}
+                  onChange={handleChange}
+                  label="Description"
+                />
+                <FormButton type="submit">Submit</FormButton>
+              </StyledForm>
+            </Modal.Content>
+            <Modal.Action onClose={handleCloseModal} />
+          </Modal>
+        </TransactionsRow>
       </Layout>
     );
   }
 }
 
 Transactions.propTypes = {
-  categoryList: PropTypes.shape({
-    categories: PropTypes.array
-  }),
-  transactionList: PropTypes.shape({
-    transactions: PropTypes.array
-  }),
-  mutate: PropTypes.func,
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      year: PropTypes.string,
-      month: PropTypes.string
-    })
-  })
+  categories: PropTypes.instanceOf(Array).isRequired,
+  transactions: PropTypes.instanceOf(Array).isRequired,
+  mutate: PropTypes.func
 };
 
 Transactions.defaultProps = {
-  categoryList: {},
-  transactionList: {},
-  mutate: () => {},
-  match: {}
+  mutate: () => {}
 };
 
-export default compose(graphql(ADD_TRANSACTION), withRouter)(Transactions);
+export default compose(
+  withRouter,
+  graphql(ADD_TRANSACTION),
+  withCategoryQuery,
+  withTransactionQuery
+)(Transactions);
