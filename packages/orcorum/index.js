ee7064126d667ff0f8e873a145e809ff33f18563
Chukwuemeka Inya <inyachukwuemeka1@gmail.com>
2020-02-03 11:14:54 +0100

    Export concat function

--- packages/orcorum/index.js
+++ packages/orcorum/index.js
@@ -2,8 +2,10 @@ import _assigned from './src/assigned';
 import _displayName from './src/displayName';
 import _chunk from './src/chunk';
 import _compact from './src/compact';
+import _concat from './src/concat';
 
 export const assigned = _assigned;
 export const displayName = _displayName;
 export const chunk = _chunk;
 export const compact = _compact;
+export const concat = _concat;
