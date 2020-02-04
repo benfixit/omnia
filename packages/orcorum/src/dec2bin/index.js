8f26ddc66830dd7f15222b4a633f03af20b10342
Chukwuemeka Inya <inyachukwuemeka1@gmail.com>
2020-02-04 23:49:10 +0100

    Add dec2bin function

---
+++ packages/orcorum/src/dec2bin/index.js
@@ -0,0 +1,11 @@
+/**
+ * Convert decimal to binary number
+ * @param num
+ * @returns {string}
+ * @private
+ */
+const _dec2bin = num => {
+  return num.toString(2);
+};
+
+export default _dec2bin;
